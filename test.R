21b6cb32657eda39ae6ad32b273633ee8e9e75a1
LEROY <leroymatheo5118@gmail.com>
2025-09-04 22:59:50 +0200

    init R , add a test file using radian and httpgd

---
+++ test.R
@@ -0,0 +1,31 @@
+# Un exemple simple de code R
+
+install.packages("remotes")
+remotes::install_github("nx10/httpgd")
+
+# install.packages("radian")
+# library(radian)
+
+# install.packages("styler")
+# library(styler)
+
+# test 
+
+styler::style_file(
+  "Test_R.R",
+  style = styler::tidyverse_style(scope = "tokens", indent_by = 2),
+  transformers = styler::tidyverse_style(scope = "tokens"),
+  width.cutoff = 80,
+)
+
+# Créer un vecteur de nombres
+nombres <- c(1, 2, 3, 4, 5)
+
+# Calculer la moyenne
+moyenne <- mean(nombres)
+
+# Afficher la moyenne
+print(paste("La moyenne est :", moyenne))
+
+# Tracer un graphique simple
+plot(nombres, type = "o", col = "blue", xlab = "Index", ylab = "Valeur", main = "Graphique des Nombres")
